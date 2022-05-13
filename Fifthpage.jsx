5dec312110f215e065f41188419492dfacca8ff8
A-D-11 <104553483+A-D-11@users.noreply.github.com>
2022-05-13 12:48:45 +0530

    Add files via upload

---
+++ Fifthpage.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+
+// This is the page for Kids Clothings 
+
+export default function Fifthpage() {
+  return (
+    <div>
+        
+     <div className="container">
+
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div className="col">
+        <div className="card shadow-sm">
+        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+         role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+         <title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/> 
+          </svg>
+
+         <div className="card-body">
+            <p className="card-text">U.S.Polo Assn.Kids</p>
+            <small className='text-muted'>Boys Cotton Casual Shirt</small>
+      </div> 
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      <div className="card-body">
+        <p className="card-text">Max</p>
+         <small className="text-muted">Boys Printed Casual Shirt </small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+       
+       
+      <div className="card-body">
+        <p className="card-text">Solly Junior</p>
+         <small className="text-muted">Ditsy Floral Casual Shirts</small>
+      </div>
+      
+    </div>
+  </div>
+
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">Natilene</p>
+         <small className="text-muted">Opaque Casual Shirt</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+     
+      <div className="card-body">
+        <p className="card-text">Pepe Jeans</p>
+         <small className="text-muted">Girls Striped Casual Shirt</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">Pspeaches</p>
+         <small className="text-muted">Floral Midi Dress</small>
+      </div>
+    </div>
+  </div>
+
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">Urbano Juniors</p>
+         <small className="text-muted">Boys Slim Fit Denim Joggers</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      <div className="card-body">
+        <p className="card-text">Vastramay</p>
+         <small className="text-muted">Boys Printed Kurti with Dhoti pajamas</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">BONKIDS</p>
+         <small className="text-muted">Boys Spiderman Slim T-shirt</small>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    </div>
+  )
+}
