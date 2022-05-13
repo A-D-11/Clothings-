53deafadb1f7a6af340e12ae244453012e2da510
A-D-11 <104553483+A-D-11@users.noreply.github.com>
2022-05-13 12:50:39 +0530

    Add files via upload

---
+++ Secondpage.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+ 
+// This is the page for the mens Shirts
+
+export default function Thirdpage() {
+  return (
+    <div>
+        
+     <div className="container">
+
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div className="col">
+        <div className="card shadow-sm">
+        <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+         role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+         <title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/> 
+          </svg>
+
+         <div className="card-body">
+            <p className="card-text">Levis</p>
+            <small className='text-muted'>Men Cotton Casual Shirt</small>
+      </div> 
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      <div className="card-body">
+        <p className="card-text">Roadster</p>
+         <small className="text-muted">Men Slim Fit Casual Shirt</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+       
+       
+      <div className="card-body">
+        <p className="card-text">HIGHLANDER</p>
+         <small className="text-muted">Men Slim Fit Casual Shirt</small>
+      </div>
+      
+    </div>
+  </div>
+
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">Calvin Klein </p>
+         <small className="text-muted">Slim Fit Checked Shirt</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+     
+      <div className="card-body">
+        <p className="card-text">U.S. Polo Assn.</p>
+         <small className="text-muted">Tailored Fit Casual Shirt</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">Roadster</p>
+         <small className="text-muted">Men Regular Fit Casual Shirt</small>
+      </div>
+    </div>
+  </div>
+
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">Levis </p>
+         <small className="text-muted">Motorcycle Collection Denim</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      <div className="card-body">
+        <p className="card-text">Tommy Hilfiger</p>
+         <small className="text-muted">Men Solid Casual Shirt</small>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card shadow-sm">
+      <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+      
+      <div className="card-body">
+        <p className="card-text">KETCH</p>
+         <small className="text-muted">Slim Fit Casual Cotton Shirt</small>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    </div>
+  )
+}
---
+++ Thirdpage.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+
+ // This is the page for the Women Jeans Clothings 
+
+export default function Secondpage() {
+  return (
+    <div>
+      <div>
+        
+        <div className="container">
+   
+           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+           <div className="col">
+           <div className="card shadow-sm">
+           <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+            role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/> 
+             </svg>
+   
+            <div className="card-body">
+               <p className="card-text">Roadster</p>
+               <small className='text-muted'>Women Mid-Rise Skinny Jeans</small>
+         </div> 
+       </div>
+     </div>
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+         <div className="card-body">
+           <p className="card-text">SASSAFRAS</p>
+            <small className="text-muted">Women Jogger Jeans</small>
+         </div>
+       </div>
+     </div>
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          
+          
+         <div className="card-body">
+           <p className="card-text">STREET9</p>
+            <small className="text-muted">Women Flared Jeans</small>
+         </div>
+         
+       </div>
+     </div>
+   
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+         
+         <div className="card-body">
+           <p className="card-text">Miss Chase</p>
+            <small className="text-muted">Women Skinny Fit Jeans</small>
+         </div>
+       </div>
+     </div>
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+        
+         <div className="card-body">
+           <p className="card-text">Status Quo</p>
+            <small className="text-muted">Women Slim Fit Jeans</small>
+         </div>
+       </div>
+     </div>
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+         
+         <div className="card-body">
+           <p className="card-text">Colt</p>
+            <small className="text-muted">Women Slim Fit Jeans</small>
+         </div>
+       </div>
+     </div>
+   
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+         
+         <div className="card-body">
+           <p className="card-text">Urbano Fashion</p>
+            <small className="text-muted">Women Slim Fit Jeans</small>
+         </div>
+       </div>
+     </div>
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+         <div className="card-body">
+           <p className="card-text">Oxemberg</p>
+            <small className="text-muted">Women Slim Fit Jeans</small>
+         </div>
+       </div>
+     </div>
+     <div className="col">
+       <div className="card shadow-sm">
+         <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+   
+         
+         <div className="card-body">
+           <p className="card-text">FREESOUL</p>
+            <small className="text-muted">Women Slim Fit Jeans</small>
+         </div>
+       </div>
+     </div>
+   </div>
+   </div>
+       </div>
+    </div>
+  )
+}
